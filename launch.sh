bcd805d5143ae444eda4e82ae81624fb29184c66
jsebfranck <jsebfranck@gmail.com>
2014-06-05 23:55:04 +0200

    CasperJS test with proxy configuration

---
+++ launch.sh
@@ -0,0 +1,9 @@
+
+# TODO launch the gatling recorder and start the record
+
+# TODO launch the e2e test
+casperjs --proxy=127.0.0.1:8000 e2e/casperjs/whatismyip.js
+
+# TODO stop the record
+
+# TODO launch the generated simulation
